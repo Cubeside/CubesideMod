9e9b4484534de0d8ce52bbd46842fd9e8dd0d15a
Brokkonaut <hannos17@gmx.de>
2025-12-16 05:40:03 +0100

    Fix game mode selection screen without op

--- src/main/java/de/fanta/cubeside/mixin/MixinGameModeSelectionScreen.java
+++ src/main/java/de/fanta/cubeside/mixin/MixinGameModeSelectionScreen.java
@@ -1,13 +1,16 @@
 package de.fanta.cubeside.mixin;
 
 import net.minecraft.client.gui.screens.debug.GameModeSwitcherScreen;
+import net.minecraft.server.permissions.PermissionCheck;
+import net.minecraft.server.permissions.PermissionSet;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameModeSwitcherScreen.class)
 public abstract class MixinGameModeSelectionScreen {
-    // FIXME 1.21.11
-    // @Redirect(method = "switchToHoveredGameMode(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/gui/screens/debug/GameModeSwitcherScreen$GameModeIcon;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;hasPermissions(I)Z"))
-    // private static boolean returnFakePermissionCheck(LocalPlayer clientPlayerEntity, int permissionLevel) {
-    // return true;
-    // }
+    @Redirect(method = "switchToHoveredGameMode(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/gui/screens/debug/GameModeSwitcherScreen$GameModeIcon;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/permissions/PermissionCheck;check(Lnet/minecraft/server/permissions/PermissionSet;)Z"))
+    private static boolean returnFakePermissionCheck(PermissionCheck check, PermissionSet permissions) {
+        return true;
+    }
 }
