49b008f2bc226d91cfc7859ed46497bc7b19f421
Brokkonaut <hannos17@gmx.de>
2025-12-16 06:43:50 +0100

    use existing server list entry for quick join if found

--- src/main/java/de/fanta/cubeside/mixin/MixinCustomTitleScreen.java
+++ src/main/java/de/fanta/cubeside/mixin/MixinCustomTitleScreen.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.screens.ConnectScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.client.multiplayer.ServerList;
 import net.minecraft.client.multiplayer.resolver.ServerAddress;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,9 +27,25 @@ public abstract class MixinCustomTitleScreen extends Screen {
     @Unique
     private ServerData selectedEntry;
 
+    @Unique
+    private ServerAddress serverAddress;
+
     @Inject(at = @At("TAIL"), method = "init")
     private void init(CallbackInfo ci) {
+        serverAddress = new ServerAddress(Configs.Generic.FastJoinButtonIP.getStringValue(), Configs.Generic.FastJoinButtonPort.getIntegerValue());
         selectedEntry = new ServerData(Configs.Generic.FastJoinButtonIP.getStringValue(), Configs.Generic.FastJoinButtonIP.getStringValue(), ServerData.Type.OTHER);
+
+        ServerList servers = new ServerList(this.minecraft);
+        servers.load();
+        int count = servers.size();
+        for (int i = 0; i < count; i++) {
+            ServerData listEntryServerData = servers.get(i);
+            ServerAddress listEntryServerAddress = ServerAddress.parseString(listEntryServerData.ip);
+            if (listEntryServerAddress.getPort() == serverAddress.getPort() && listEntryServerAddress.getHost().equalsIgnoreCase(serverAddress.getHost())) {
+                selectedEntry = listEntryServerData;
+                serverAddress = listEntryServerAddress;
+            }
+        }
     }
 
     @Inject(at = @At("HEAD"), method = "createNormalMenuOptions")
@@ -36,7 +53,7 @@ public abstract class MixinCustomTitleScreen extends Screen {
         if (!Configs.Generic.FastJoinButtonText.getStringValue().isBlank() && !Configs.Generic.FastJoinButtonIP.getStringValue().isBlank()) {
             this.addRenderableWidget(Button.builder(Component.literal(Configs.Generic.FastJoinButtonText.getStringValue()), button -> {
                 if (selectedEntry != null) {
-                    ConnectScreen.startConnecting(this, Minecraft.getInstance(), new ServerAddress(Configs.Generic.FastJoinButtonIP.getStringValue(), Configs.Generic.FastJoinButtonPort.getIntegerValue()), selectedEntry, false, null);
+                    ConnectScreen.startConnecting(this, Minecraft.getInstance(), serverAddress, selectedEntry, false, null);
                 }
             }).bounds(this.width / 2 - 100 + 205, y + spacingY, 80, 20).build());
         }
